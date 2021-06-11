a964f5d6e7517f97e519e96df88e112dbcc04fb9
Sangeeth Mathew John <65032296+sangethmathewjohn@users.noreply.github.com>
2021-06-11 14:24:21 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+import wordlist
+import random
+import arts
+import os
+
+gameover = False
+disword =""
+display =[]
+flag = 0
+dis =[]
+
+word =random.choice(wordlist.wordlist)
+length =len(word)
+missing1 =random.randint(0,length-1)
+missing2 =random.randint(0,length-1)
+
+for l in word:
+  dis += l
+  display += '_'
+display[missing1] = dis[missing1]
+display[missing2] = dis[missing2]
+
+for j in display:
+    disword += j +" "
+
+while not gameover:
+  
+  os.system('clear') or os.system('cls')
+  print(arts.logo+"\n")
+  print(f"Find the MISSING letter in the word:\n{disword}")
+  print(f"{arts.stages[flag]}")
+  guess =input("Guess the Letter : ").lower()
+  disword=""
+
+  if not guess in word:
+    flag +=1
+  if guess in display:
+    flag +=1
+  
+  for i in range(length):
+    if word[i]== guess:
+      display[i] = guess
+  for d in display:
+    disword += d + " "
+    
+  if dis==display or (flag > len(arts.stages)-2):
+      gameover = True   
+  else:
+      gameover = False
+    
+
+if dis == display:
+  print(f"Congrats!!! You found the letters in {word}")
+else:
+  print(f"You dids not found  the letter in the {word}")
